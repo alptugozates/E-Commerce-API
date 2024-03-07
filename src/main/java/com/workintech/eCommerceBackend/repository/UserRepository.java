6ea9a78b5b96713869ff7d8aa1867b7189c2da33
alptugozates <omeralptugozates@gmail.com>
2024-03-07 23:41:12 +0300

    User için findByEmail methodu ekler query yazdırır

--- src/main/java/com/workintech/eCommerceBackend/repository/UserRepository.java
+++ src/main/java/com/workintech/eCommerceBackend/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.workintech.eCommerceBackend.repository;
 
 import com.workintech.eCommerceBackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(String email);
 }
